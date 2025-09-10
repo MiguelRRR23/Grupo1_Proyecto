166c5ab68131591330877b99aba259d9064e3d66
MiguelRRR23 <miguelrrr816@gmail.com>
2025-09-10 01:21:21 -0500

    Add files via upload
    Primer commit

---
+++ Grupo1_Proyecto_S2/Proyecto_S2/src/Lab/Evaluacion.java
@@ -0,0 +1,16 @@
+package Lab;
+
+public class Evaluacion {
+
+private double n1, n2, n3, n4, prom;
+
+public Evaluacion(double n1, double n2, double n3, double n4, double prom) {
+	this.n1 = n1;
+	this.n2 = n2;
+	this.n3 = n3;
+	this.n4 = n4;
+	this.prom = prom;
+}
+
+
+}
---
+++ Grupo1_Proyecto_S2/Proyecto_S2/src/Lab/V1.java
@@ -0,0 +1,106 @@
+package Lab;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+
+public class V1 extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_1;
+	private JLabel lblNewLabel_2;
+	private JLabel lblNewLabel_3;
+	private JTextField txtN1;
+	private JTextField txtN2;
+	private JTextField txtN3;
+	private JTextField txtN4;
+	private JScrollPane scrollPane;
+	private JTextArea txtS;
+	private JButton btnP;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					V1 frame = new V1();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public V1() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		lblNewLabel = new JLabel("Nota 1");
+		lblNewLabel.setBounds(10, 24, 45, 13);
+		contentPane.add(lblNewLabel);
+		
+		lblNewLabel_1 = new JLabel("Nota 2");
+		lblNewLabel_1.setBounds(10, 47, 45, 13);
+		contentPane.add(lblNewLabel_1);
+		
+		lblNewLabel_2 = new JLabel("Nota 3");
+		lblNewLabel_2.setBounds(205, 24, 45, 13);
+		contentPane.add(lblNewLabel_2);
+		
+		lblNewLabel_3 = new JLabel("Nota 4");
+		lblNewLabel_3.setBounds(205, 47, 45, 13);
+		contentPane.add(lblNewLabel_3);
+		
+		txtN1 = new JTextField();
+		txtN1.setBounds(54, 21, 96, 19);
+		contentPane.add(txtN1);
+		txtN1.setColumns(10);
+		
+		txtN2 = new JTextField();
+		txtN2.setBounds(54, 44, 96, 19);
+		contentPane.add(txtN2);
+		txtN2.setColumns(10);
+		
+		txtN3 = new JTextField();
+		txtN3.setBounds(248, 21, 96, 19);
+		contentPane.add(txtN3);
+		txtN3.setColumns(10);
+		
+		txtN4 = new JTextField();
+		txtN4.setBounds(248, 44, 96, 19);
+		contentPane.add(txtN4);
+		txtN4.setColumns(10);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 130, 416, 123);
+		contentPane.add(scrollPane);
+		
+		txtS = new JTextArea();
+		scrollPane.setViewportView(txtS);
+		
+		btnP = new JButton("Calcular Promedio");
+		btnP.setBounds(129, 89, 121, 31);
+		contentPane.add(btnP);
+
+	}
+}
